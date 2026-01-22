6b92d016ae8ab13768fcdec21dd143504d49e517
Miles-Kurth <mkurth9612@student.pps.net>
2026-01-22 15:18:20 -0800

    worked a bit

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ https://education.lego.com/en-us/support/mindstorms-ev3/building-instructions#ro
 """
 
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor, GyroSensor
+from pybricks.ev3devices import Motor, GyroSensor, ColorSensor
 from pybricks.parameters import Port,Stop
 from pybricks.tools import wait
 from pybricks.robotics import DriveBase
@@ -28,7 +28,7 @@ class LaserSensor:
 
     def distance(self):
         now = time.time()
-        if now - self.last_time > 0.1:
+        if now - self.last_time > 0.05:
             self.last_time = now
             results = self.i2c.read(0x42, 2)
             self.last_dist = results[0] + (results[1] << 8)
@@ -43,6 +43,11 @@ left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 arm_motor = Motor(Port.A)
 
+# Initialize sensors
+laser_sensor = LaserSensor(Port.S1)
+color_sensor = ColorSensor(Port.S3)
+gyro_sensor = GyroSensor(Port.S4)
+
 # Declare variables
 armDirection = "not set"
 targetAngle = 0 # degrees
@@ -55,10 +60,6 @@ speed = 200 # mm/s
 # touch the ground.
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104) #104 -> 119?
 
-# Initialize sensors
-laser_sensor = LaserSensor(Port.S1)
-gyro_sensor = GyroSensor(Port.S4)
-
 # At start
 ev3.speaker.set_volume(20); #ev3.speaker.beep(660,200)
 #print(arm_motor.angle())
@@ -70,13 +71,11 @@ def armTurnRight():
     global armDirection; armDirection = "Right"
     arm_motor.run_until_stalled(-500,then=Stop.HOLD,duty_limit=40)
     wait(100)
-
     
 def armTurnLeft():
     global armDirection; armDirection = "Left"
     arm_motor.run_until_stalled(500,then=Stop.HOLD,duty_limit=40)
     wait(100)
-
 
 def armTurnForwardRight():
     armTurnRight()
@@ -84,7 +83,6 @@ def armTurnForwardRight():
     wait(400)
     arm_motor.reset_angle(0)
     arm_motor.run_target(-100,60,then=Stop.HOLD,wait=True)
-
 
 def armTurnForwardLeft():
     armTurnLeft()
@@ -95,11 +93,10 @@ def armTurnForwardLeft():
 
 
 def printLaserDistance():
-    print(armDirection + ": " + str(laser_sensor.distance()) + "mm")
+    print("Laser distance: " + str(laser_sensor.distance()) + "mm")
 
 def printGyroAngle():
-    print("Gyro: ",end='')
-    print(gyro_sensor.angle())
+    print("Gyro: " + str(gyro_sensor.angle()) + "°")
 
 def turn90Degrees(degrees):
     gyro_sensor.reset_angle(0)
@@ -119,3 +116,69 @@ def turn90Degrees(degrees):
     printGyroAngle()
 
 
+def start():
+    gyro_sensor.reset_angle(0)
+    left_motor.reset_angle(0)
+    right_motor.reset_angle(0)
+    wait(10)
+
+def startTurnDynamic(speed):
+    if (speed > 0):
+        left_motor.run(1 * speed)
+        right_motor.run(-0 * speed)
+    else:
+        left_motor.run(0 * speed)
+        right_motor.run(-1 * speed)
+    
+
+def driveToCan():
+    printLaserDistance()
+    robot.drive(1000,0)
+    while (laser_sensor.distance() > 110):
+        printLaserDistance()
+    wait(100)
+    robot.brake()
+
+
+
+# Code below
+
+start()
+
+canWidth = 50
+canAngle = 0
+canDistance = 0
+
+startTurnDynamic(400)
+while True:
+    printLaserDistance()
+    # printGyroAngle()
+    # print(left_motor.angle())
+    # print(time.time())
+    if (laser_sensor.distance() < 400):
+        printLaserDistance()
+        canAngle = gyro_sensor.angle()
+        canDistance = laser_sensor.distance()
+        robot.brake()
+        break
+wait(300)
+print(str(canAngle) + " " + str(gyro_sensor.angle()))
+
+startTurnDynamic(-100)
+while (laser_sensor.distance() > canDistance + 25):
+    printGyroAngle()
+    printLaserDistance()
+    if (abs(canDistance - laser_sensor.distance()) < 5):
+        robot.brake()
+        break
+
+driveToCan()
+
+
+# Gyro and laser test
+# while True:
+#     printGyroAngle()
+#     printLaserDistance()
+
+
+wait(1000)
